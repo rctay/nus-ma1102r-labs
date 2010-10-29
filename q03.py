8173410d284c41bebda69886f0bcb4d5f8643a30
Tay Ray Chuan <rctay89+samsung@gmail.com>
2010-10-29 17:23:47 +0800

    q03/py: add G() implementation

--- q03.py
+++ q03.py
@@ -14,6 +14,15 @@ class PrimeChecker(object):
 					for num in re.split('\s+', line):
 						self.prime_dict[int(num)] = 1
 
+def G(num, prime_func):
+	count = 0
+	for i in range(1, num/2+1):
+		other = num - i
+		if prime_func(i) and prime_func(other):
+			print "%d + %d = %d" % (i, other, num)
+			count += 1
+	return count
+
 if __name__ == '__main__':
 	inst = PrimeChecker()
 	inst.load("primes.txt")
